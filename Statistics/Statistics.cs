a83b79130008aacda46d1bc1ed74cc4b8d837f9c
gopinathvanapalli <115626278+gopinathvanapalli@users.noreply.github.com>
2022-10-18 22:11:07 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -34,12 +34,7 @@ namespace Statistics
         
     }
     
-    public class Stats
-    {
-        public float average {get;set;}
-        public float min {get;set;}
-        public float max {get;set;}
-    }
+    
     
      
 }
