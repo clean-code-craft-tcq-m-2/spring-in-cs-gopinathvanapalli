43bdaf45053e70d17a53e3157af4fd030d23b4cd
gopinathvanapalli <115626278+gopinathvanapalli@users.noreply.github.com>
2022-10-18 22:37:21 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -5,7 +5,7 @@ namespace Statistics
 {
     public class StatsComputer
     {
-        static float average, min, max = 0.0;
+        static float average, min, max = 0f;
         public Stats CalculateStatistics(List<float> numbers) 
         {
             int sum=0;
@@ -16,13 +16,12 @@ namespace Statistics
             numbers = numbers.Sort();
             
             
-            if(numbers.Any())
-            {
+            
                 foreach(int num in numbers)
                 {
                     sum += num;
                 }
-            }
+           
             
             s.average = (sum) / numbers.Count;
             s.min = numbers[0];
