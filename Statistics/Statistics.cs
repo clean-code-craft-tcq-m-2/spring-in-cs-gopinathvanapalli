c2e89abd6035a090aa54ba12539c44fa41e2e12d
gopinathvanapalli <115626278+gopinathvanapalli@users.noreply.github.com>
2022-10-19 22:07:11 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -16,7 +16,7 @@ namespace Statistics
                 return s;
             }
             
-            numbers = numbers.Sort();
+            numbers.Sort();
             
             
             
